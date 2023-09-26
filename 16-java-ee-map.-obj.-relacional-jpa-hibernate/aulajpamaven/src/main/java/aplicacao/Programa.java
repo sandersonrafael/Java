eeeb9ce216a81a2897012c739f3ea49a742dbdf0
= <=>
2023-09-26 15:29:29 -0300

    JPA - Hibernate - projeto básico configurado e exemplos

--- 16-java-ee-map.-obj.-relacional-jpa-hibernate/aulajpamaven/src/main/java/aplicacao/Programa.java
+++ 16-java-ee-map.-obj.-relacional-jpa-hibernate/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -1,19 +1,50 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Carlim Tete", "carlimte@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Marcele Campelo", "marcecam@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Dudu Laerte", "dudula@gmail.com");
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        /* Operacao 1 - Objetos criados inicialmente */
+        /* Pessoa p1 = new Pessoa(null, "Carlim Tete", "carlimte@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Marcele Campelo", "marcecam@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Dudu Laerte", "dudula@gmail.com"); */
 
-        // Criar base de dados no MySQL Workbench:
-        // CREATE DATABASE aulajpa;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
 
-        // Criar projeto MAVEN também
+        /* Operacao 1 - Objetos inseridos inicialmente */
+        /* em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit(); */
+
+        /* Operacao 2 - Recuperação de objeto */
+        // passa a classe como primeiro argumento e a Pk como segundo argumento
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+
+        /* Operação 3 - Monitoramento e deleção */
+        Pessoa p2 = new Pessoa(2, null, null);
+        // em.remove(p2);
+        /* mesmo que p2 seja idêntico a algum objeto do Banco de Dados, ele não é o mesmo
+        ou seja, ele não está sendo monitoriado e vai dar erro.
+        para apagar, precisa ter buscado anteriormente ou ter criado a própria pessoa e salvo no DB
+        antes de usar o método remove(pessoa) */
+
+        // como Pessoa p acima realmente foi buscada, é possível usar para remover, pois está monitorada
+
+        // sempre que fizer alteração ou algo que não seja uma consulta, é preciso iniciar e finalizar uma transação:
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+
+        System.out.println("Sucesso!");
+        em.close();
+        emf.close();
     }
 }
--- 16-java-ee-map.-obj.-relacional-jpa-hibernate/aulajpamaven/src/main/java/dominio/Pessoa.java
+++ 16-java-ee-map.-obj.-relacional-jpa-hibernate/aulajpamaven/src/main/java/dominio/Pessoa.java
@@ -2,7 +2,16 @@ package dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
